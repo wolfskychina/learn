25ec19d44d5ee271e087d51fdc9fdd8af22fbd38
wolfskychina <wolfskychina@126.com>
2019-04-01 17:20:33 +0800

    office

--- src/stuc/queue/QueueExample.java
+++ src/stuc/queue/QueueExample.java
@@ -1,9 +1,9 @@
-import java.util.Deque;
+package stuc.queue;
+
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
-public class Main {
+public class QueueExample {
 
     public static void main(String[] args) {
 
