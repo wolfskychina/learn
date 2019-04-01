2b510c0756b3179f58a7ea8b046f18c2162330a7
wolfskychina <wolfskychina@126.com>
2019-04-01 16:53:02 +0800

    office

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!git test");
+
+
+        Stack<String> s1 = new Stack<String>();
+
+        s1.push("111" );
+        s1.push("222");
+        s1.push("333");
+
+        for(int i = s1.size();i>0;i--){
+
+            System.out.println(s1.size());
+            System.out.println(s1.pop());
+
+
+        }
+
+
     }
 }
