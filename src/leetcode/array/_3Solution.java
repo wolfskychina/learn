05f43be62da60be27868ef5a68e96d7e0f406d70
wolfsky <wolfskychina@126.com>
2019-11-10 23:25:23 +0800

    ll

--- src/leetcode/array/_3Solution.java
+++ src/leetcode/array/_3Solution.java
@@ -9,21 +9,23 @@ import java.util.*;
 public class _3Solution {
 
     public int lengthOfLongestSubstring(String s) {
-        // int n = s.length();
-        Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0;
-        //i :left  j:right
 
-        while (i <  s.length() && j < s.length()) {
-            // try to extend the range [i, j]
-            if (!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                ans = Math.max(ans, j - i);
-            }
-            else {
+        Set<Character> set = new HashSet<>();
+
+        int ans =0;
+        int i = 0;
+        int j =0;
+
+        while(i<s.length() &&j<s.length()){
+
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                ans = Math.max(ans,j-i);
+            }else{
                 set.remove(s.charAt(i++));
             }
         }
+
         return ans;
     }
 
