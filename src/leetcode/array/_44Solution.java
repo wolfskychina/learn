3ec9adb5efb6a59555e572ccdbdd518aeca622ba
wolfsky <wolfskychina@126.com>
2020-01-06 23:26:03 +0800

    home

--- src/leetcode/array/_44Solution.java
+++ src/leetcode/array/_44Solution.java
@@ -2,7 +2,7 @@ package leetcode.array;
 
 public class _44Solution {
 
-    public boolean isMath(String s, String p){
+    public boolean isMatch(String s, String p){
         int i = 0, j =0, iStar = -1, jStar = -1,m = s.length(), n = p.length();
         while(i<m){
             if(j<n && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')){
@@ -14,10 +14,18 @@ public class _44Solution {
             }else if(iStar >= 0) {
                 i = ++iStar;
                 j = jStar +1;
+                // 发生了i和j不匹配的情况，但是iStar不是负值
+                // 说明有一个有效的*可以吃掉这个不匹配
+                //
             }else return false;
         }
 
         while(j<n && p.charAt(j) == '*') ++j;
         return  j==n;
+    }
+
+    public static void main(String[] args){
+        _44Solution so = new _44Solution();
+        so.isMatch("eradbckts","er*cks");
     }
 }
