1f512ec96786a64fdcdb1aba929c72b010f10197
wolfsky <wolfskychina@126.com>
2020-01-14 21:54:54 +0800

    home

--- src/leetcode/array/_45Solution.java
+++ src/leetcode/array/_45Solution.java
@@ -6,10 +6,20 @@ public class _45Solution {
         for (int i = 0; i < A.length - 1; i++) {
             curFarthest = Math.max(curFarthest, i + A[i]);
             if (i == curEnd) {
+                // 有点类似于广度优先
+                // curEnd代表本次跳跃能够到达最远位置
                 jumps++;
                 curEnd = curFarthest;
             }
         }
         return jumps;
+    }
+
+
+    public static void main(String[] args){
+
+        _45Solution so = new _45Solution();
+        int[] a = {2,3,1,1,4};
+        so.jump(a);
     }
 }
