72213f4fe52b0d8c6ae7763e9769401f52157e01
wolfskychina <wolfskychina@126.com>
2020-01-17 16:18:50 +0800

    office

---
+++ src/leetcode/array/_22Solution.java
@@ -0,0 +1,34 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _22Solution {
+
+    public int max = 0;
+
+    public List<String> generateParenthesis(int n){
+        this.max = n;
+        List<String> ans = new ArrayList<>();
+
+        backtrace(ans, "" , 0,0);
+        return ans;
+    }
+
+    private void backtrace(List<String> ans, String cur, int open, int close){
+
+        if(cur.length() == max*2) {
+            ans.add(cur);
+            return;
+        }
+
+        if(open<max){
+            backtrace(ans,cur+"(",open +1,close);
+
+        }
+
+        if(close<open){
+            backtrace(ans, cur+")",open,close+1);
+        }
+    }
+}
---
+++ src/leetcode/array/_23Solution.java
@@ -0,0 +1,46 @@
+package leetcode.array;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class _23Solution {
+    public class ListNode{
+        public int val;
+        public ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode mergeLists(List<ListNode> lists){
+
+        if(null == lists || 0== lists.size()) return null;
+
+        Queue<ListNode> queue = new PriorityQueue<>(lists.size(), new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                if(o1.val == o2.val) return 0;
+                if(o1.val < o2.val) return -1;
+                return 1;
+            }
+        }
+        );
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while(!queue.isEmpty()){
+
+            tail.next = queue.poll();
+            tail = tail.next;
+
+            if(null!= tail.next){
+                queue.offer(tail.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
