8f73da6c8ec375bdc8f3f0b802f2dd5176c1b612
wolfskychina <wolfskychina@126.com>
2020-01-21 17:18:12 +0800

    office

---
+++ src/leetcode/array/_17Solution.java
@@ -0,0 +1,29 @@
+package leetcode.array;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class _17Solution {
+
+    public List<String> letterCombination(String digits){
+
+        LinkedList<String> ans =new LinkedList<>();
+        String[] mapping = new String[]{"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        if(digits.length() == 0) return ans;
+
+        for(int i=0;i<digits.length();i++){
+
+            int x=Character.getNumericValue(digits.charAt(i));
+
+            while(ans.peek() == null || ans.peek().length()==i){
+                String t = ans.peek() ==null?"":ans.peek();
+                for(char a: mapping[i].toCharArray()){
+                    ans.add(t+a);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
