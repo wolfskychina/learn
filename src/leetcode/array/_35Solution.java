a61ea5cbbac1219fbaa496232c6536e889da5e8c
wolfsky <wolfskychina@126.com>
2020-01-28 22:34:50 +0800

    home

--- src/leetcode/array/_35Solution.java
+++ src/leetcode/array/_35Solution.java
@@ -21,7 +21,7 @@ public class _35Solution {
 
     public static void main(String[] args){
         _35Solution so = new _35Solution();
-        int[] nums = {0,1,2,3,4,5,6,7,8,9,10};
+        int[] nums = {0,4,4,4,4,5,6,7,8,9,10};
         System.out.println(so.searchInsert(nums,4));
     }
 }
