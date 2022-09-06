5a5cd3b0a6d711bbe8118b9b6c93836b0a3193cc
wolfsky <wolfskychina@126.com>
2022-09-06 10:01:35 +0800

    office

--- src/stuc/bst/BstOperation.java
+++ src/stuc/bst/BstOperation.java
@@ -72,23 +72,6 @@ public class BstOperation {
         }
     }
 
-    /**
-     * First non-retrival tree walk 
-     * @param Node x
-     */
-    public static void firstTreeWalk(Node x){
-        if(x ==null) return ;
-
-        Stack<Node> s = new Stack<>();
-        s.push(x);
-
-        while(!s.isEmpty()){
-            Node node = s.pop();
-            System.out.println(node.getValue());
-            if(node.getLeft()!=null) s.push(node.getLeft());
-            if(node.getRight()!=null) s.push(node.getRight());
-        }
-    }
 
     public static void main(String[] args) {
 
