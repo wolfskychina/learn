633068d11483542f5d57bd4c94e858129fa901f4
wolfskychina <wolfskychina@126.com>
2022-09-08 21:47:39 +0800

    home

--- src/stuc/bst/BinaryTree.java
+++ src/stuc/bst/BinaryTree.java
@@ -17,8 +17,8 @@ public class BinaryTree {
         while(!s.isEmpty()){
             Node node = s.pop();
             System.out.println(node.getValue());
-            if(node.getLeft()!=null) s.push(node.getLeft());
             if(node.getRight()!=null) s.push(node.getRight());
+            if(node.getLeft()!=null) s.push(node.getLeft());
         }
     }
 
