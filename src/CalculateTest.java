041b1109849b871b5ffc970691faf3d270b6890b
wolfsky <wolfskychina@126.com>
2023-06-25 09:15:33 +0800

    office

---
+++ src/CalculateTest.java
@@ -0,0 +1,18 @@
+public class CalculateTest {
+   
+    
+    public static void main(String[] args){
+
+        int j=0;
+        for(int i =0;i<100;i++){
+
+            j = j++;
+        }
+
+        int a =5;
+        System.out.println("the value is :"+ (a<5? 9.1:9));
+
+        System.out.println(j);
+
+    }
+}
