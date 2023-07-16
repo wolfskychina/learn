d21733b25cde8d8f88c9aa67ca6716a68f4fb8d7
wolfskychina <wolfskychina@126.com>
2023-07-16 23:59:02 +0800

    home

---
+++ src/leetcode/array/_199Solution.java
@@ -0,0 +1,57 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 输出从树的右侧的视图
+ * 层次遍历输出每层的最后一个节点
+ */
+public class _199Solution {
+
+    /**
+     * Definition for a binary tree node.
+    */
+    public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res =new ArrayList<>();
+        if(root == null) return res ;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        TreeNode right = new TreeNode();
+
+
+        while(!queue.isEmpty()){
+
+            int qlength = queue.size();
+            for(int i=0;i<qlength;i++){
+
+                TreeNode node = queue.poll();
+                if(node.left !=null ) queue.offer(node.left);
+                if(node.right !=null ) queue.offer(node.right);
+                right = node;
+            }
+
+            res.add(right.val);
+
+        }
+
+        return res;
+    }
+}
