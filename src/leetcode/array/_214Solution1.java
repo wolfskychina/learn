d00a75578ed5864ac4f3b96999741c766ef521b5
wolfskychina <wolfskychina@126.com>
2023-07-24 00:33:27 +0800

    home

--- src/leetcode/array/_214Solution1.java
+++ src/leetcode/array/_214Solution1.java
@@ -3,22 +3,21 @@ package leetcode.array;
 /**
  * 一个字符串，最少在前面补充多少个字符可以变成回文串
  * 输出这个最短的回文串
- * on2的解法，超时了
+ * 开始看错题了，以为前后补充字符都可以
  */
-public class _214Solution {
+public class _214Solution1 {
    
-    public String shortestPalindrome(String s) {
+    public String shortestPalindrome1(String s) {
 
         if(s==null || s.equals("")) return s;
         int min = Integer.MAX_VALUE;
         char[] word = s.toCharArray();
         // 对每个字母循环
-        // word.length -1 /2 保证是在左边补充字符
-        for(int i =0 ;i<= (word.length-1)/2; i++) {
+        for(int i =0 ;i< word.length; i++) {
         // 判断以某一个字符为中心是否能够补全成为回文串
         // 回文串需要判断两次，分别是以字符为中心和以当前两个字符为中心进行判断
         int len1 = ifPalindrome(word, i,i);
-        int len2 = ifPalindrome(word, i-1, i);
+        int len2 = ifPalindrome(word, i, i+1);
 
         // 如果能够补全，则计算需要补全的长度，和当前最小值比较
         if(len1>=0) min = Math.min(min,len1);
@@ -39,22 +38,51 @@ public class _214Solution {
             sb.append(s.charAt(length-1-i));
         }
         sb.append(s);
-        return sb.toString();
+        String re = sb.toString();
+        if(isPalindrome(re)) return re;
 
+        sb = new StringBuilder();
+        sb.append(s);
+
+        for(int i=min-1;i>=0;i--){
+            sb.append(s.charAt(i));
+        }
+        re = sb.toString();
+
+        return re;
+
+    }
+
+    private boolean isPalindrome(String word){
+
+        char[] array = word.toCharArray();
+        for(int i =0;i<array.length/2;i++){
+            if(array[i] != array[array.length -i-1])
+                return false;
+        }
+
+        return true;
     }
 
     private int ifPalindrome(char[] word, int l, int r){
             boolean odd ;
-            odd = l == r;
+            if(l==r){
+                odd = true;
+            }else{
+                odd = false;
+            }
             int count = 0;
 
             while(l>=0 && r<word.length){
+
 
                 if(word[l] != word[r]) return -1;
                 count++;
                 l--;
                 r++;
             }
+
+            if(l == -1 && r == word.length) return 0;
 
             if(odd){
                 return word.length - 2*count +1;
@@ -66,7 +94,7 @@ public class _214Solution {
 
     public static void main(String[] args){
 
-        _214Solution so = new _214Solution();
-        System.out.println(so.shortestPalindrome("abb"));
+        _214Solution1 so = new _214Solution1();
+        System.out.println(so.shortestPalindrome1("ba"));
     }
 }
