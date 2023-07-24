39e3aa533bacff39a5c17579b7762d1928e1716a
wolfskychina <wolfskychina@126.com>
2023-07-25 00:25:52 +0800

    home

--- src/leetcode/array/_217Solution.java
+++ src/leetcode/array/_217Solution.java
@@ -13,7 +13,6 @@ public class _217Solution {
      * @return
      */
     public boolean containsDuplicate(int[] nums) {
-        int s = 0;
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == nums[i + 1]) return true;
