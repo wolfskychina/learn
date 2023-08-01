a6f067faf8bbedc061f5ce49a828a790b8c7e278
wolfsky <wolfskychina@126.com>
2023-08-01 16:56:01 +0800

    office

--- src/leetcode/array/_222Solution.java
+++ src/leetcode/array/_222Solution.java
@@ -93,22 +93,3 @@ public class _222Solution {
     }
 
 }
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
---
+++ src/leetcode/array/_225Solution.java
@@ -0,0 +1,57 @@
+package leetcode.array;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 使用两个队列实现栈的功能
+ */
+public class _225Solution {
+  
+    class MyStack {
+
+        private Queue<Integer> q;
+        private Queue<Integer> q1;
+    
+        public MyStack() {
+    
+            q = new  LinkedList<>();
+            q1 = new LinkedList<>();
+        }
+        
+        public void push(int x) {
+    
+            int tmp;
+            while(!q.isEmpty()){
+    
+                tmp = q.poll();
+                q1.offer(tmp);
+            }
+            q.offer(x);
+            while(!q1.isEmpty()){
+    
+                tmp = q1.poll();
+                q.offer(tmp);
+            }
+            
+        }
+        
+        public int pop() {
+
+            return q.poll();
+            
+        }
+        
+        public int top() {
+    
+            return q.peek();
+            
+        }
+        
+        public boolean empty() {
+    
+                return q.isEmpty();
+            
+        }
+    }
+}
