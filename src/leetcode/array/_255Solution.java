95ae79dc47ed4c5d8b070fac9100915bd0c976b3
wolfsky <wolfskychina@126.com>
2023-08-10 11:12:49 +0800

    office

---
+++ src/leetcode/array/_255Solution.java
@@ -0,0 +1,25 @@
+package leetcode.array;
+
+import java.util.Stack;
+/**
+ * 校验一个bst的先根遍历数组是否正确
+ */
+public class _255Solution {
+    /**
+     * 用栈模拟bst的先根遍历
+     * @param preorder
+     * @return
+     */
+    public boolean verifyPreorder(int[] preorder) {
+        int low = Integer.MIN_VALUE;
+        Stack<Integer> path = new Stack<>();
+        for (int p : preorder) {
+            if (p < low)
+                return false;
+            while (!path.empty() && p > path.peek())
+                low = path.pop();
+            path.push(p);
+        }
+        return true;
+    } 
+}
