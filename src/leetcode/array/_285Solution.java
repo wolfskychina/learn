c293e221e4ffd4b0550f41036bab911317c63551
wolfsky <wolfskychina@126.com>
2023-08-14 17:46:44 +0800

    office

--- src/leetcode/array/_285Solution.java
+++ src/leetcode/array/_285Solution.java
@@ -1,7 +1,34 @@
 package leetcode.array;
 
+import leetcode.util.TreeNode;
+
 /**
- * 求一个二叉树中某个节点中序遍历的后继节点
+ * 求一个BST树中某个节点中序遍历的后继节点
  */
 public class _285Solution {
+
+    public TreeNode findNext(TreeNode root, TreeNode p){
+
+        if(p.right!=null){
+            p = p.right;
+            while(p.left!=null){
+                p = p.left;
+            }
+            return p;
+        }
+
+        // 需要向上判断
+        // 注意p有可能是最大的节点，没有后继节点
+        TreeNode next = null;
+        while(root.val!=p.val){
+            if(root.val > p.val){
+                next = root;
+                root = root.left;
+            }else{
+                root = root.right;
+            }
+        }
+        return next;
+
+    }
 }
