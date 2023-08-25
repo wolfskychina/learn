8c09b43661248e5fbfeeb8a95273de953c250383
wolfsky <wolfskychina@126.com>
2023-08-25 17:42:29 +0800

    office

---
+++ src/leetcode/a300/_279Solution.java
@@ -0,0 +1,24 @@
+package leetcode.a300;
+/**
+ * 和相加等于n的最少的平方数的组合
+ */
+public class _279Solution {
+
+    /**
+     * 动态规划
+     * @param n
+     * @return
+     */
+    public int numSquares(int n) {
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int minn = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                minn = Math.min(minn, f[i - j * j]);
+            }
+            f[i] = minn + 1;
+        }
+        return f[n];
+    }
+    
+}
