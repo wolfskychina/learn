7ddba8e44c53c025528435981fe969a2174eab96
wolfsky <wolfskychina@126.com>
2023-09-06 15:42:18 +0800

    office

---
+++ src/leetcode/a0/_17Solution.java
@@ -0,0 +1,29 @@
+package leetcode.a0;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class _17Solution {
+
+    public List<String> letterCombination(String digits){
+
+        LinkedList<String> ans =new LinkedList<>();
+        String[] mapping = new String[]{"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        if(digits.length() == 0) return ans;
+
+        for(int i=0;i<digits.length();i++){
+
+            int x=Character.getNumericValue(digits.charAt(i));
+
+            while(ans.peek() == null || ans.peek().length()==i){
+                String t = ans.peek() ==null?"":ans.remove();
+                for(char a: mapping[x].toCharArray()){
+                    ans.add(t+a);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
