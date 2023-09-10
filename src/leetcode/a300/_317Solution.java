2e4f6fefbdbfbc143684b2697ad18a38aa9532cb
wolfskychina <wolfskychina@126.com>
2023-09-10 12:19:02 +0800

    home

--- src/leetcode/a300/_317Solution.java
+++ src/leetcode/a300/_317Solution.java
@@ -76,7 +76,7 @@ public class _317Solution {
             steps++;
         }
         // 提前终止条件，原图无解，避免最后再判断，解决时间
-        // 两个建筑不能连接，此路不通
+        // x,y节点本身无法到达某个其他的建筑节点
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j] && grid[i][j] == 1) {
