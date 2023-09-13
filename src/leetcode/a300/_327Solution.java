a2f43d910293bd842eb49fee1cb7fd5abaa1e9dc
wolfskychina <wolfskychina@126.com>
2023-09-13 19:58:27 +0800

    office

--- src/leetcode/a300/_327Solution.java
+++ src/leetcode/a300/_327Solution.java
@@ -60,6 +60,73 @@ public class _327Solution {
         return ans;
     }
 
+    /**
+     * 运用归并排序
+     * 将前缀数组进行归并排序，前缀数组的顺序并不会影响可能的区间和的数量
+     * 每一趟归并的时候，左右两组前缀和的差的可能，所有趟加起来正好遍历了所有前缀和的差得可能
+     * @param nums
+     * @param lower
+     * @param upper
+     * @return
+     */
+    public int countRangeSum2(int[] nums, int lower, int upper) {
+        long s = 0;
+        long[] sum = new long[nums.length + 1];
+        for (int i = 0; i < nums.length; ++i) {
+            s += nums[i];
+            sum[i + 1] = s;
+        }
+        return countRangeSumRecursive(sum, lower, upper, 0, sum.length - 1);
+    }
+
+    public int countRangeSumRecursive(long[] sum, int lower, int upper, int left, int right) {
+        if (left == right) {
+            return 0;
+        } else {
+            int mid = (left + right) / 2;
+            int n1 = countRangeSumRecursive(sum, lower, upper, left, mid);
+            int n2 = countRangeSumRecursive(sum, lower, upper, mid + 1, right);
+            int ret = n1 + n2;
+
+            // 首先统计下标对的数量
+            int i = left;
+            int l = mid + 1;
+            int r = mid + 1;
+            while (i <= mid) {
+                while (l <= right && sum[l] - sum[i] < lower) {
+                    l++;
+                }
+                while (r <= right && sum[r] - sum[i] <= upper) {
+                    r++;
+                }
+                ret += r - l;
+                i++;
+            }
+
+            // 随后合并两个排序数组
+            long[] sorted = new long[right - left + 1];
+            int p1 = left, p2 = mid + 1;
+            int p = 0;
+            while (p1 <= mid || p2 <= right) {
+                if (p1 > mid) {
+                    sorted[p++] = sum[p2++];
+                } else if (p2 > right) {
+                    sorted[p++] = sum[p1++];
+                } else {
+                    if (sum[p1] < sum[p2]) {
+                        sorted[p++] = sum[p1++];
+                    } else {
+                        sorted[p++] = sum[p2++];
+                    }
+                }
+            }
+            for (int j = 0; j < sorted.length; j++) {
+                sum[left + j] = sorted[j];
+            }
+            return ret;
+        }
+    }
+
     public static void main(String[] args) {
         _327Solution so = new _327Solution();
         //so.countRangeSum(a, -57577, 18133);
