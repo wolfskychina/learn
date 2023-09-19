91396d689df698d3a85cdea85477c1e777fbc39f
wolfsky <wolfskychina@126.com>
2023-09-19 17:27:44 +0800

    office

--- src/leetcode/a300/_347Solution.java
+++ src/leetcode/a300/_347Solution.java
@@ -8,11 +8,12 @@ import java.util.Map;
 
 /**
  * 出现频率最高的k个元素
+ * 返回不要求按照频率排序
  */
 public class _347Solution {
 
     /**
-     * 基于选择排序
+     * 基于快速排序中的分划操作
      * 
      * @param nums
      * @param k
@@ -24,12 +25,14 @@ public class _347Solution {
             occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
         }
         // 获取每个数字出现次数
+        // map转换到list中，方便进行分划的操作
         List<int[]> values = new ArrayList<int[]>();
         for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
             int num = entry.getKey(), count = entry.getValue();
             values.add(new int[] { num, count });
         }
         int[] ret = new int[k];
+        // 使用了全闭区间
         qsort(values, 0, values.size() - 1, ret, 0, k);
         return ret;
     }
@@ -41,6 +44,7 @@ public class _347Solution {
 
         int pivot = values.get(start)[1];
         int index = start;
+        // i快指针、index慢指针
         for (int i = start + 1; i <= end; i++) {
             // 使用双指针把不小于基准值的元素放到左边，
             // 小于基准值的元素放到右边
@@ -61,6 +65,7 @@ public class _347Solution {
                 ret[retIndex++] = values.get(i)[0];
             }
             if (k > index - start + 1) {
+                // 右侧还有一部分（ k-(index-start+1) )大的元素需要继续查找
                 qsort(values, index + 1, end, ret, retIndex, k - (index - start + 1));
             }
         }
---
+++ src/leetcode/a300/_348Solution.java
@@ -0,0 +1,88 @@
+package leetcode.a300;
+/**
+ * 348. Design Tic-Tac-Toe
+ * 给定一系列步骤，判断最后谁赢
+ */
+public class _348Solution {
+
+    char[][] cell;
+
+    public _348Solution(int n) {
+        cell = new char[n][n];
+    }
+
+    /**
+     * 只需要判断该元素所在的行列和对角线
+     * @param row
+     * @param col
+     * @param player
+     * @return
+     */
+    public int move(int row, int col, int player) {
+
+        char c = (player == 1) ? 'X' : '0';
+        cell[row][col] = c;
+        boolean r1 = true, r2 = true, r3 = true, r4 = true;
+        for (int i = 0; i < cell.length; i++) {
+            if (cell[row][i] != c)
+                r1 = false;
+            if (cell[i][col] != c)
+                r2 = false;
+            if ((row != col) || cell[i][i] != c)
+                r3 = false;
+            if ((row + col != cell.length - 1) || cell[i][cell.length - i - 1] != c)
+                r4 = false;
+        }
+
+        if (r1 | r2 | r3 | r4) {
+
+            return player;
+        } else {
+
+            return 0;
+        }
+
+    }
+
+    /**
+     * O(1)的解法
+     * 只有某列或者某行或者对象线的和是n或者-n的时候，有人获胜
+     */
+    public class TicTacToe {
+        int[] rows;
+        int[] cols;
+        int diagonal;
+        int antiDiagonal;
+     
+        public TicTacToe(int n) {
+            rows = new int[n];
+            cols = new int[n];
+        }
+     
+        public int move(int row, int col, int player) {
+            int currentPlayer = (player == 1) ? 1 : -1;
+            // 更新 rows 和 cols 数组中的当前用户
+            rows[row] += currentPlayer;
+            cols[col] += currentPlayer;
+            // 更新 diagonal
+            if (row == col) {
+                diagonal += currentPlayer;
+            }
+            // 更新 antiDiagonal
+            if (col == (cols.length - row - 1)) {
+                antiDiagonal += currentPlayer;
+            }
+            int n = rows.length;
+            // 检查玩家是否赢得游戏
+            if (Math.abs(rows[row]) == n ||
+                    Math.abs(cols[col]) == n ||
+                    Math.abs(diagonal) == n ||
+                    Math.abs(antiDiagonal) == n) {
+                return player;
+            }
+            // 没有人赢
+            return 0;
+        }
+     }
+     
+}
