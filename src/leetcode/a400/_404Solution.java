d505ad1bd7c245ecb40f7aa414e10f3e9eb287dc
wolfskychina <wolfskychina@126.com>
2023-10-08 23:26:58 +0800

    home

---
+++ src/leetcode/a400/_404Solution.java
@@ -0,0 +1,23 @@
+package leetcode.a400;
+
+import leetcode.util.TreeNode;
+/**
+ * 二叉树中所有左叶子节点（是父节点的左儿子的叶子结点）的和
+ */
+public class _404Solution {
+   
+    int sum=0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null) return 0;
+
+        if(root.left!=null&&root.left.left==null&&root.left.right==null){
+            sum+= root.left.val; 
+        }
+
+        sumOfLeftLeaves(root.left);
+        sumOfLeftLeaves(root.right);
+
+        return sum;
+
+    }
+}
---
+++ src/leetcode/a400/_405Solution.java
@@ -0,0 +1,19 @@
+package leetcode.a400;
+
+public class _405Solution {
+
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 7; i >= 0; i--) {
+            int val = (num >> (4 * i)) & 0xf;
+            if (sb.length() > 0 || val > 0) {
+                char digit = val < 10 ? (char) ('0' + val) : (char) ('a' + val - 10);
+                sb.append(digit);
+            }
+        }
+        return sb.toString();
+    }
+}
