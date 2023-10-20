b4b7846ddabcd0a1110b8e367d6f83c737146828
wolfskychina <wolfskychina@126.com>
2023-10-21 00:21:16 +0800

    home

--- src/leetcode/a400/_457Solution.java
+++ src/leetcode/a400/_457Solution.java
@@ -13,19 +13,19 @@ public class _457Solution {
 
             if (failSet.contains(i))
                 continue;
-            if(i==(i + nums[i] % nums.length+nums.length) % nums.length)
+            // 关键，两个地方都需要取余(i + nums[i] % nums.length + nums.length) % nums.length
+            if (i == (i + nums[i] % nums.length + nums.length) % nums.length)
                 continue;
             Set<Integer> tmpSucc = new HashSet<>();
-            int idx = (i + nums[i] % nums.length +nums.length) % nums.length;
-            int flag = nums[i]>0?1:-1;
+            int idx = (i + nums[i] % nums.length + nums.length) % nums.length;
+            int flag = nums[i] > 0 ? 1 : -1;
             tmpSucc.add(i);
             while (true) {
 
-                if ((nums[idx] > 0 && flag>0) || 
-                        (nums[idx] < 0 && flag<0)) 
-                        {
-                    int newidx = (idx + nums[idx] % nums.length+nums.length) % nums.length;
-                    if (failSet.contains(newidx)||idx ==newidx) {
+                if ((nums[idx] > 0 && flag > 0) ||
+                        (nums[idx] < 0 && flag < 0)) {
+                    int newidx = (idx + nums[idx] % nums.length + nums.length) % nums.length;
+                    if (failSet.contains(newidx) || idx == newidx) {
                         for (int node : tmpSucc) {
                             failSet.add(node);
                         }
@@ -38,7 +38,6 @@ public class _457Solution {
                     }
 
                 } else {
-
                     for (int node : tmpSucc) {
                         failSet.add(node);
                     }
@@ -53,10 +52,50 @@ public class _457Solution {
 
     }
 
-    public static void main(String[] args){
+    /**
+     * 快慢指针
+     * @param nums
+     * @return
+     */
+    public boolean circularArrayLoop1(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                continue;
+            }
+            int slow = i, fast = next(nums, i);
+            // 判断非零且方向相同
+            while (nums[slow] * nums[fast] > 0 && nums[slow] * nums[next(nums, fast)] > 0) {
+                if (slow == fast) {
+                    if (slow != next(nums, slow)) {
+                        return true;
+                    } else {
+                        break;
+                    }
+                }
+                slow = next(nums, slow);
+                fast = next(nums, next(nums, fast));
+            }
+            int add = i;
+            while (nums[add] * nums[next(nums, add)] > 0) {
+                int tmp = add;
+                add = next(nums, add);
+                nums[tmp] = 0;
+            }
+        }
+        return false;
+    }
+
+    public int next(int[] nums, int cur) {
+        int n = nums.length;
+        return ((cur + nums[cur]) % n + n) % n; // 保证返回值在 [0,n) 中
+    }
+
+
+    public static void main(String[] args) {
 
         _457Solution so = new _457Solution();
-        int[] array = new int[]{-2,-3,-9};
+        int[] array = new int[] { -2, -3, -9 };
         so.circularArrayLoop(array);
     }
 }
