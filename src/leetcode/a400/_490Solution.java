78b342d97623ede1c261fc9443d217db5436b5ee
wolfskychina <wolfskychina@126.com>
2023-10-27 23:56:00 +0800

    home

--- src/leetcode/a400/_490Solution.java
+++ src/leetcode/a400/_490Solution.java
@@ -1,27 +1,55 @@
 package leetcode.a400;
 
+/**
+ * 小球走迷宫，小球只有碰到墙壁才会停下，随后开始下一轮选择方向然后滚动
+ * 小球停下的时候才可能走到出口，滚动路径上有出口也不算走到
+ */
 public class _490Solution {
-   
+
+    int[][] step = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+    /**
+     * 深度优先搜索
+     * 
+     * @param maze
+     * @param start
+     * @param destination
+     * @return
+     */
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        return dfs(maze, start[0],start[1],destination);
-        
+        return dfs(maze, start[0], start[1], destination);
+
     }
 
     private boolean dfs(int[][] maze, int i, int j, int[] destination) {
         boolean res = false;
-        for(int k=0;k<4;k++){
+        maze[i][j] = 2;
+        for (int k = 0; k < 4; k++) {
 
-            // 临时的访问把经过的节点设置为2
-            int[] nextPoint = findEndAndAddSet(k,i,j);
+            // 寻找四个方向的节点
+            int xx = i;
+            int yy = j;
+            // xx+step[k][0]>=0 && xx+step[k][0]<=maze.length && yy+step[k][1]>=0 &&
+            // yy+step[k][1]<=maze[0].length
+            while (xx + step[k][0] >= 0 && xx + step[k][0] < maze.length && yy + step[k][1] >= 0 &&
+                    yy + step[k][1] < maze[0].length && maze[xx + step[k][0]][yy + step[k][1]] != 1) {
+                xx += step[k][0];
+                yy += step[k][1];
+            }
+
             // 如果遇到2的节点直接continue，遇到了回路
-            // 判断停下的点是否是球门,如果是就返回true
-            // 判断停下的是不是节点2，如果是返回false
-            // 如果都不是，就从该节点继续开始下一层的遍历
-            // 
-            res |= dfs(maze,visited, nextPoint[0],nextPoint[1],destination);
+            if (maze[xx][yy] == 2)
+                continue;
 
-            
+            // 判断停下的点是否是球门,如果是就返回true
+            if (xx == destination[0] && yy == destination[1])
+                return true;
+
+            // 如果都不是，就从该节点继续开始下一层的遍历
+            res |= dfs(maze, xx, yy, destination);
+
         }
+
         return res;
     }
 }
