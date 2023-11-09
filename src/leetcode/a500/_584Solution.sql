9148de714f154da999f0dd4dded36fe170d3ec4c
wolfskychina <wolfskychina@126.com>
2023-11-10 00:07:04 +0800

    home

---
+++ src/leetcode/a500/_584Solution.sql
@@ -0,0 +1,15 @@
+-- 筛选不是某个人推荐的所有用户
+-- 注意推荐栏是null的情况
+Table: Customer
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are not referred by the customer with id = 2.
