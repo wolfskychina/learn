8ea83ae1b04f499ac6c6d258f911496c39c3a3dc
wolfsky <wolfskychina@126.com>
2023-12-11 12:30:58 +0800

    office

--- src/leetcode/a600/_679Solution.java
+++ src/leetcode/a600/_679Solution.java
@@ -1,28 +1,88 @@
 package leetcode.a600;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 /**
  * 24点游戏
  * 4个原始数字是可以换位置的
  */
 public class _679Solution {
 
+    List<int[]> list = new ArrayList<>();
+
     public boolean judgePoint24(int[] cards) {
 
-        int num[][] = new int[4][2];
-        for (int i = 0; i < num.length; i++) {
-            num[i][0] = cards[i];
-            num[i][1] = 1;
+        //
+        Set<Integer> set = new HashSet<>();
+        set.add(0);
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i : set) {
+            q.offer(i);
+        }
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            int idx = q.poll();
+            int seq[] = new int[4];
+            seq[0] = cards[idx];
+            set.remove(idx);
+            perform(1, cards, set, seq);
+            set.add(idx);
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int[] card : list) {
 
-            // 四种运算
-            for (int j = 1; j <= 4; j++) {
-                if (compute4(num, i, j))
-                    return true;
+            int num[][] = new int[4][2];
+            for (int i = 0; i < num.length; i++) {
+                num[i][0] = card[i];
+                num[i][1] = 1;
             }
 
+            for (int i = 0; i < 3; i++) {
+
+                // 四种运算
+                for (int j = 1; j <= 4; j++) {
+                    if (compute4(num, i, j))
+                        return true;
+                }
+
+            }
         }
+
         return false;
+    }
+
+    private void perform(int toFill, int[] cards, Set<Integer> set, int[] seq) {
+
+        if (toFill == 3) {
+            for (int i : set) {
+                seq[3] = cards[i];
+            }
+            int[] re = Arrays.copyOf(seq, 4);
+            list.add(re);
+        } else {
+            Queue<Integer> q = new ArrayDeque<>();
+            for (int i : set) {
+                q.offer(i);
+            }
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int idx = q.poll();
+                seq[toFill] = cards[idx];
+                set.remove(idx);
+                perform(toFill + 1, cards, set, seq);
+                set.add(idx);
+            }
+        }
+
     }
 
     private boolean compute4(int[][] num, int i, int j) {
@@ -82,7 +142,8 @@ public class _679Solution {
             return false;
 
         int newnum[] = dealtwonum(num[i], num[i + 1], j);
-        if (newnum[0] / newnum[1] == 24)
+        if ((newnum[0] / newnum[1] == 24)&&
+        (newnum[0] % newnum[1] == 0))
             return true;
         return false;
     }
@@ -111,5 +172,11 @@ public class _679Solution {
             return res;
         }
         return res;
+    }
+
+    public static void main(String[] args){
+        _679Solution so = new _679Solution();
+        int [] cards = {1,1,7,7};
+        so.judgePoint24(cards);
     }
 }
