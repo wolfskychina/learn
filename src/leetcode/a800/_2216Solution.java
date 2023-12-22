74c8c2d8f84dd8bd7416da1001cac713760854f4
wolfsky <wolfskychina@126.com>
2023-12-22 19:19:57 +0800

    office

---
+++ src/leetcode/a800/_2216Solution.java
@@ -0,0 +1,33 @@
+package leetcode.a800;
+/**
+ * 形成美观数组所需要去掉的最少元素的数量
+ * 美观数组是指所有的奇数位和后面的偶数位均不相等
+ */
+public class _2216Solution {
+   
+    /**
+     * {greedy}
+     * @param nums
+     * @return
+     */
+     public int minDeletion(int[] nums) {
+
+        if(nums.length==1) return 1;
+        int l=0;
+        int r=1;
+        int count =0;
+        while(l<nums.length&&r<nums.length){
+
+            while(r<nums.length&&nums[l]==nums[r]){
+                r++;
+                count++;
+            }
+
+            l=r+1;
+            r=l+1;
+
+        }
+
+        return (nums.length-count)%2==0?count:count+1;
+    }
+}
