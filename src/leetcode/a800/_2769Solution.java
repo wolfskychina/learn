8f69306c698403d89a0157b602bfdf453a876abd
wolfskychina <wolfskychina@126.com>
2023-12-24 12:06:02 +0800

    home

---
+++ src/leetcode/a800/_2769Solution.java
@@ -0,0 +1,12 @@
+package leetcode.a800;
+/**
+ * 最大可达成的数字
+ * {easy}
+ */
+public class _2769Solution {
+   
+    public int theMaximumAchievableX(int num, int t) {
+
+        return num+2*t;
+    }
+}
