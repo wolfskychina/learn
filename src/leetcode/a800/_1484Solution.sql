42afa8abc7b802773b18cbe6302cf271a29e915b
wolfskychina <wolfskychina@126.com>
2024-01-01 00:30:00 +0800

    home

---
+++ src/leetcode/a800/_1484Solution.sql
@@ -0,0 +1,24 @@
+-- 组内同列拼接 {sql:group_concat()}
+Input: 
+Activities table:
++------------+------------+
+| sell_date  | product     |
++------------+------------+
+| 2020-05-30 | Headphone  |
+| 2020-06-01 | Pencil     |
+
+Output: 
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+
+
+SELECT sell_date,
+       COUNT(DISTINCT product) num_sold,
+       GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') products
+
+FROM Activities
+GROUP BY sell_date;
