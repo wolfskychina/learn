91aff6db73de248c84f5b69b2549e6e13dd4191e
wolfskychina <wolfskychina@126.com>
2024-01-05 00:42:59 +0800

    home

---
+++ src/leetcode/a800/_1005Solution.java
@@ -0,0 +1,30 @@
+package leetcode.a800;
+
+import java.util.Arrays;
+
+public class _1005Solution {
+
+    public int largestSumAfterKNegations(int[] nums, int k) {
+
+        Arrays.sort(nums);
+        int i = 0;
+        while (i < nums.length && nums[i] < 0 && k != 0) {
+            nums[i] = -nums[i];
+            i++;
+            k--;
+        }
+        if (k != 0) {
+            if ((i == nums.length || nums[i] != 0) && k % 2 == 1) {
+                Arrays.sort(nums);
+                nums[0] = -nums[0];
+            }
+        }
+
+        int sum = 0;
+        for (int j : nums) {
+            sum += j;
+        }
+
+        return sum;
+    }
+}
