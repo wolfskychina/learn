125456260023346ac681fa1cb72a82900d9005fe
wolfskychina <wolfskychina@126.com>
2024-01-07 00:09:30 +0800

    home

---
+++ src/leetcode/a800/_2661Solution.java
@@ -0,0 +1,35 @@
+package leetcode.a800;
+/**
+ * 找出叠涂元素
+ */
+public class _2661Solution {
+
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+
+        int[] m = new int[mat.length];
+        int[] n = new int[mat[0].length];
+        int[][] map = new int[arr.length + 1][2];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+
+                map[mat[i][j]][0] = i;
+                map[mat[i][j]][1] = j;
+            }
+        }
+
+        int i = 0;
+        while (i < arr.length) {
+
+            m[map[arr[i]][0]]++;
+            n[map[arr[i]][1]]++;
+
+            if (m[map[arr[i]][0]] == mat[0].length ||
+                    n[map[arr[i]][1]] == mat.length)
+                return i;
+
+            i++;
+        }
+
+        return i;
+    }
+}
