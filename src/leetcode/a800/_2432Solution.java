ae2b126bd92a5a056f5c0d5169c4732eb556b026
wolfskychina <wolfskychina@126.com>
2024-01-14 00:34:15 +0800

    home

---
+++ src/leetcode/a800/_2432Solution.java
@@ -0,0 +1,22 @@
+package leetcode.a800;
+/**
+ * 处理用时最长的任务
+ * TODO
+ */
+public class _2432Solution {
+   
+    public int hardestWorker(int n, int[][] logs) {
+        int res = logs[0][0];
+        int max = logs[0][1];
+        for(int i=1;i<logs.length;i++){
+            if(logs[i][1]-logs[i-1][1]>max){
+                max = logs[i][1]-logs[i-1][1];
+                res =logs[i][0];
+            }else if(logs[i][1]-logs[i-1][1]==max && res>logs[i][0]){
+                res = logs[i][0];
+            }
+        }
+        
+        return res;
+    }
+}
