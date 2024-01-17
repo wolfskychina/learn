0d2a628425ebad458000835584e5f4a9065dda5d
wolfskychina <wolfskychina@126.com>
2024-01-17 23:35:02 +0800

    home

---
+++ src/leetcode/a0/_11Solution.java
@@ -0,0 +1,19 @@
+package leetcode.a0;
+/**
+ * 盛水最多的容器
+ * {greedy},{two pointers}
+ */
+public class _11Solution {
+   
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+}
