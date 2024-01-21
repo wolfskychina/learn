15ba9bf82208056453310660d90f6c0ada58bf0a
wolfskychina <wolfskychina@126.com>
2024-01-21 22:16:27 +0800

    home

---
+++ src/leetcode/a100/_170Solution.java
@@ -0,0 +1,49 @@
+package leetcode.a100;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 设计一个判断是否有两个元素之和等于value的方法
+ */
+public class _170Solution {
+   
+    class TwoSum {
+
+        private Map<Integer, Integer> cntMap;
+    
+        private int max;
+    
+        private int min;
+    
+        public TwoSum() {
+            this.cntMap = new HashMap<>();
+            this.max = Integer.MIN_VALUE;
+            this.min = Integer.MAX_VALUE;
+        }
+    
+        public void add(int number) {
+            cntMap.put(number, cntMap.getOrDefault(number, 0) + 1);
+            min = Math.min(min, number);
+            max = Math.max(max, number);
+        }
+    
+        public boolean find(int value) {
+            if ((max << 1) < value || (min << 1) > value) {
+                return false;
+            }
+            for (Map.Entry<Integer, Integer> entry : cntMap.entrySet()) {
+                int diff = value - entry.getKey();
+                if (diff == entry.getKey()) {
+                    if (entry.getValue() > 1) {
+                        return true;
+                    }
+                } else {
+                    if (cntMap.containsKey(diff)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/leetcode/a800/_1475Solution.java
@@ -0,0 +1,35 @@
+package leetcode.a800;
+/**
+ * 商品折扣后的最终价格
+ * {monotonic stack}
+ */
+public class _1475Solution {
+   
+    public int[] finalPrices(int[] prices) {
+        int [] idx = new int[prices.length];
+        int [] value = new int[prices.length];
+        int j=0;
+        value[0] = prices[0];
+        idx[0] = 0;
+        
+        for(int i=1;i<prices.length;i++){
+               
+                while(j>=0 && prices[i]<=value[j]){
+                    prices[idx[j]] -= prices[i];
+                    j--;
+                }
+            
+            value[++j] = prices[i];
+            idx[j] = i;
+                
+        }
+        return prices;
+
+    }
+
+    public static void main(String[] args){
+        _1475Solution so = new _1475Solution();
+        int[] prices = {10,1,1,6};
+        so.finalPrices(prices);
+    }
+}
