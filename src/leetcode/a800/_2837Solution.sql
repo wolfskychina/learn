ad9e63ce88d6e2b7a26ae26d1f3ea83f62c2ac52
wolfsky <wolfskychina@126.com>
2024-01-26 19:52:53 +0800

    office

--- src/leetcode/a800/_2837Solution.sql
+++ src/leetcode/a800/_2837Solution.sql
@@ -1,3 +1,4 @@
 -- 统计所有旅客的总旅行里程
-select user_id ,name ,sum(distance) as 'traveled distance' from 
-( select  Users.user_id,name  ,if(distance is null, 0,distance) as distance from Users left join Rides on Users.user_id = Rides.user_id ) as a group by user_id order by user_id;
+select user_id ,name ,if(distance is null, 0,distance) as 'traveled distance' from
+( select Users.user_id, name,distance from Users left join 
+(  select user_id , sum(distance) as distance from  Rides group by user_id ) as b   on Users.user_id = b.user_id  ) as c order by user_id;
