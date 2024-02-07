f85b4e253cab405bec05d4a29a33ebcd49e1217a
wolfsky <wolfskychina@126.com>
2024-02-07 18:26:27 +0800

    office

---
+++ src/leetcode/a800/_1389Solution.java
@@ -0,0 +1,26 @@
+package leetcode.a800;
+
+import java.util.LinkedList;
+import java.util.List;
+/**
+ * 按照既定顺序创建目标数组
+ * 
+ */
+public class _1389Solution {
+
+    public int[] createTargetArray(int[] nums, int[] index) {
+
+        List<Integer> list = new LinkedList<>();
+        int i = 0;
+        while (i < nums.length) {
+            // list中间插入元素
+            list.add(index[i], nums[i]);
+            i++;
+        }
+        int[] ans = new int[nums.length];
+        for (int j = 0; j < ans.length; j++) {
+            ans[j] = list.get(j);
+        }
+        return ans;
+    }
+}
---
+++ src/leetcode/a800/_1394Solution.java
@@ -0,0 +1,20 @@
+package leetcode.a800;
+/**
+ * 找出数组中的幸运数
+ * {easy}
+ */
+public class _1394Solution {
+   
+    public int findLucky(int[] arr) {
+
+        int count[] = new int[501];
+        for(int i:arr){
+            count[i]++;
+        }
+        for(int i=count.length-1;i>=1;i--){
+            if(count[i]==i)
+                return i;
+        }
+        return -1;
+    }
+}
