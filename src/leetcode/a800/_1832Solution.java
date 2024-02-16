c430079f115ee7e87b8fee2c98363455a322513a
wolfskychina <wolfskychina@126.com>
2024-02-17 03:04:20 +0800

    home

---
+++ src/leetcode/a800/_1832Solution.java
@@ -0,0 +1,22 @@
+package leetcode.a800;
+/**
+ * 句子是否为全字母句子
+ */
+public class _1832Solution {
+   
+    public boolean checkIfPangram(String sentence) {
+
+        int [] arr = new int[26];
+        for(char c:sentence.toCharArray()){
+
+            arr[c-'a']++;
+        }
+        for(int i=0;i<26;i++){
+
+            if(arr[i]==0) 
+                return false;
+        }
+
+        return true;
+    }
+}
