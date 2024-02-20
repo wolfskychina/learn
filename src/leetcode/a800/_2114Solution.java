d5e6e6057c77d23c8272bfbab91a0b79eff9a0ec
wolfsky <wolfskychina@126.com>
2024-02-20 19:39:27 +0800

    office

---
+++ src/leetcode/a800/_2114Solution.java
@@ -0,0 +1,19 @@
+package leetcode.a800;
+/**
+ * 最多单词的句子
+ * {easy}
+ */
+public class _2114Solution {
+   
+    public int mostWordsFound(String[] sentences) {
+
+        int max = 0;
+        for(String str:sentences){
+
+            String[] arr = str.split(" ");
+            max = Math.max(max,arr.length);
+        }
+
+        return max;
+    }
+}
