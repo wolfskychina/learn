61e8654d231d4b670b472cef6f7f02f5b8ce0664
wolfsky <wolfskychina@126.com>
2024-02-23 20:26:08 +0800

    office

--- src/leetcode/a0/_39Solution.java
+++ src/leetcode/a0/_39Solution.java
@@ -15,7 +15,6 @@ public class _39Solution {
         return list;
     }
 
-
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain,int start){
         if(remain <0 ) return ;
         else if (remain == 0) list.add(new ArrayList<>(tempList));
