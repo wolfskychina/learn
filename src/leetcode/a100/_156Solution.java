190adbd57101497eeaf2de331d48ace6a2d0260e
wolfsky <wolfskychina@126.com>
2024-03-21 18:14:47 +0800

    office

---
+++ src/leetcode/a100/_156Solution.java
@@ -0,0 +1,24 @@
+package leetcode.a100;
+
+import leetcode.util.TreeNode;
+/**
+ * 每层上下翻转二叉树
+ * {binary tree}
+ * @param root
+ * @return
+ */
+public class _156Solution {
+   
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode parent = null, parent_right = null;
+        while(root != null){
+            TreeNode root_left = root.left;
+            root.left = parent_right;
+            parent_right = root.right;
+            root.right = parent;
+            parent = root;
+            root = root_left;
+        }
+        return parent;
+    }F
+}
