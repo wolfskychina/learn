93035e1ad4d911c70094c391abb68846972e2c52
wolfskychina <wolfskychina@126.com>
2024-06-03 21:28:06 +0800

    home

--- src/leetcode/a400/_405Solution.java
+++ src/leetcode/a400/_405Solution.java
@@ -12,6 +12,7 @@ public class _405Solution {
         for (int i = 7; i >= 0; i--) {
             int val = (num >> (4 * i)) & 0xf;
             if (sb.length() > 0 || val > 0) {
+                // 每次取4位二进制数字进行转换,恰好转成一个十六进制位
                 char digit = val < 10 ? (char) ('0' + val) : (char) ('a' + val - 10);
                 sb.append(digit);
             }
