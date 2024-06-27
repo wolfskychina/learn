25e12f94a1889750ff9e200c45880cc1700d5120
wolfsky <wolfskychina@126.com>
2024-06-27 17:50:34 +0800

    office

--- src/leetcode/a600/_618Solution.sql
+++ src/leetcode/a600/_618Solution.sql
@@ -19,12 +19,10 @@ Output:
 | Jane    | null | null   |
 +---------+------+--------+
 
-
 select 
-   max(case when continent = 'America' then name end) as America,
+    max(case when continent = 'America' then name end) as America,
     max(case when continent = 'Asia' then name end) as Asia,
     max(case when continent = 'Europe' then name end) as Europe
-
 from 
 (
     select *, row_number() over (partition by continent order by name) rk 
--- src/leetcode/a600/_620Solution.sql
+++ src/leetcode/a600/_620Solution.sql
@@ -1,3 +1,3 @@
--- 
+-- {easy} 
 select * from Cinema 
 where id%2=1 and description != 'boring' order by rating desc;
