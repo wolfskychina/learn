617238152ceadf05e7d12c3c89e942fde03983b9
wolfsky <wolfskychina@126.com>
2024-07-04 17:48:05 +0800

    office

--- src/leetcode/a600/_679Solution.java
+++ src/leetcode/a600/_679Solution.java
@@ -174,6 +174,48 @@ public class _679Solution {
         return res;
     }
 
+    private static final double EPS = 1e-6;
+    /**
+     * 直接使用backtrace
+     * {backtrace}
+     * @param A
+     * @param n
+     * @return
+     */
+    private boolean backtrack(double[] A, int n) {
+        if(n == 1) return Math.abs(A[0] - 24) < EPS;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                double a = A[i], b = A[j];
+                A[j] = A[n-1];
+                A[i] = a + b;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = a - b;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = b - a;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = a * b;
+                if(backtrack(A, n - 1)) return true;
+                if(Math.abs(b) > EPS) {
+                    A[i] = a / b;
+                    if(backtrack(A, n - 1)) return true;
+                }
+                if(Math.abs(a) > EPS) {
+                    A[i] = b / a;
+                    if(backtrack(A, n - 1)) return true;
+                }
+                A[i] = a; A[j] = b;
+            }
+        }
+        return false;
+    }
+    public boolean judgePoint241(int[] nums) {
+        double[] A = new double[nums.length];
+        for(int i = 0; i < nums.length; i++) A[i] = nums[i];
+        return backtrack(A, A.length);
+    }
+
+
     public static void main(String[] args){
         _679Solution so = new _679Solution();
         int [] cards = {1,1,7,7};
