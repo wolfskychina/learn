8273ea4e1f7fb4a9ca36b84a6e9cefdd602c3366
wolfsky <wolfskychina@126.com>
2024-07-25 17:43:26 +0800

    office

--- src/leetcode/a1000/_1450Solution.java
+++ src/leetcode/a1000/_1450Solution.java
@@ -1,16 +1,17 @@
 package leetcode.a1000;
+
 /**
  * 指定时间内做作业的学生数量
  * {easy}
  */
 public class _1450Solution {
-   
+
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
 
-        int count=0;
-        for(int i=0;i<startTime.length;i++){
+        int count = 0;
+        for (int i = 0; i < startTime.length; i++) {
 
-            if(queryTime>=startTime[i]&&queryTime<=endTime[i])
+            if (queryTime >= startTime[i] && queryTime <= endTime[i])
                 count++;
         }
         return count;
