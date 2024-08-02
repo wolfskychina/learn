0796fc9a4f9f8ef1ce31efbe27c783d19b24bd0f
wolfsky <wolfskychina@126.com>
2024-08-02 20:39:01 +0800

    home

--- src/leetcode/a1000/_1957Solution.java
+++ src/leetcode/a1000/_1957Solution.java
@@ -1,24 +1,25 @@
 package leetcode.a1000;
+
 /**
  * 删除长度超过3的相同字符组成的子串
  * {easy}
  */
 public class _1957Solution {
-   
+
     public String makeFancyString(String s) {
 
         StringBuilder sb = new StringBuilder();
-        char[] arr= s.toCharArray();
-        int l=0,r=0;
-        while(r<arr.length){
+        char[] arr = s.toCharArray();
+        int l = 0, r = 0;
+        while (r < arr.length) {
 
-            while(r<arr.length&&arr[l]==arr[r]){
+            while (r < arr.length && arr[l] == arr[r]) {
                 r++;
             }
-            for(int i=0;i<Math.min(2,r-l);i++){
+            for (int i = 0; i < Math.min(2, r - l); i++) {
                 sb.append(arr[l]);
             }
-            l=r;
+            l = r;
         }
         return sb.toString();
     }
