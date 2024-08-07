fda203f7f40b5d88fb381fe9e25fa8be9b1c9963
wolfsky <wolfskychina@126.com>
2024-08-07 20:53:22 +0800

    home

--- src/leetcode/a2000/_2210Solution.java
+++ src/leetcode/a2000/_2210Solution.java
@@ -1,4 +1,5 @@
 package leetcode.a2000;
+
 /**
  * 统计数组中峰和谷的数量
  * {easy}
@@ -7,24 +8,24 @@ public class _2210Solution {
 
     public int countHillValley(int[] nums) {
 
-        int l=1,r=1;
-        int count =0;
-        while(l<nums.length-1){
+        int l = 1, r = 1;
+        int count = 0;
+        while (l < nums.length - 1) {
 
-            while(r<nums.length&&nums[r]==nums[l])
+            while (r < nums.length && nums[r] == nums[l])
                 r++;
             r--;
-            if(l>0&&r+1<nums.length&&nums[l-1]>nums[l]&&nums[r]<nums[r+1]){
+            if (l > 0 && r + 1 < nums.length && nums[l - 1] > nums[l] && nums[r] < nums[r + 1]) {
                 count++;
             }
-            if(l>0&&r+1<nums.length&&nums[l-1]<nums[l]&&nums[r]>nums[r+1]){
+            if (l > 0 && r + 1 < nums.length && nums[l - 1] < nums[l] && nums[r] > nums[r + 1]) {
                 count++;
             }
-            l = r+1;
+            l = r + 1;
             r = l;
         }
 
         return count;
 
-    } 
+    }
 }
