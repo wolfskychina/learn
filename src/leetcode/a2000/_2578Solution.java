eace2ce3169ccc0d9f41366f52e6e5f62369b313
wolfsky <wolfskychina@126.com>
2024-08-13 21:11:03 +0800

    office

--- src/leetcode/a2000/_2578Solution.java
+++ src/leetcode/a2000/_2578Solution.java
@@ -2,6 +2,7 @@ package leetcode.a2000;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * 最小和的分割
  * {easy}
@@ -11,24 +12,24 @@ public class _2578Solution {
     public int splitNum(int num) {
         List<Integer> list = new ArrayList<>();
 
-        while(num!=0){
-            list.add(num%10);
-            num/=10;
+        while (num != 0) {
+            list.add(num % 10);
+            num /= 10;
         }
 
-        list.sort((o1, o2) -> o1-o2);
+        list.sort((o1, o2) -> o1 - o2);
 
-        int a1=0;
-        int a2 =0;
-        for(int i=0;i<list.size();i++){
+        int a1 = 0;
+        int a2 = 0;
+        for (int i = 0; i < list.size(); i++) {
 
-            if(i%2==0){
-                a1 = a1*10+list.get(i);
-            }else{
-                a2 = a2*10+list.get(i);
+            if (i % 2 == 0) {
+                a1 = a1 * 10 + list.get(i);
+            } else {
+                a2 = a2 * 10 + list.get(i);
             }
         }
 
-        return a1+a2;
+        return a1 + a2;
     }
 }
