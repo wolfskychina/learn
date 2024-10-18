26f9bb57585255f93b82d90489e646f8933b9931
wolfskychina <wolfskychina@126.com>
2024-10-18 22:15:41 +0800

    home

---
+++ src/leetcode/a1500/_1535Solution.java
@@ -0,0 +1,22 @@
+package leetcode.a1500;
+
+/**
+ * 找出数组游戏的赢家
+ * 不断将第一个元素和其他的元素比较，连续赢k次的是最后的赢家
+ * 
+ */
+public class _1535Solution {
+   
+    public int getWinner(int[] arr, int k) {
+        int mx = arr[0];
+        int win = 0;
+        for (int i = 1; i < arr.length && win < k; i++) {
+            if (arr[i] > mx) { 
+                mx = arr[i];
+                win = 0;
+            }
+            win++; 
+        }
+        return mx;
+    }
+}
---
+++ src/leetcode/a1500/_1559Solution.java
@@ -0,0 +1,49 @@
+package leetcode.a1500;
+
+/**
+ * 二维网格图中探测是否有环
+ * {matrix},{dfs}
+ */
+public class _1559Solution {
+
+    boolean[][] visited;
+    char[][] grid;
+    int m, n;
+    boolean hasRing;
+
+    public boolean containsCycle(char[][] grid) {
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
+        visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    dfs(i, j, grid[i][j], 'L');
+                    if (hasRing)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void dfs(int i, int j, char ch, char from) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != ch) {
+            return;
+        }
+        if (visited[i][j]) {
+            hasRing = true;
+            return;
+        }
+        visited[i][j] = true;
+        if (from != 'L')
+            dfs(i, j - 1, ch, 'R');
+        if (from != 'R')
+            dfs(i, j + 1, ch, 'L');
+        if (from != 'U')
+            dfs(i - 1, j, ch, 'D');
+        if (from != 'D')
+            dfs(i + 1, j, ch, 'U');
+    }
+}
