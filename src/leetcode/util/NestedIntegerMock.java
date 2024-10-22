ce6fa2912c7bd7ba52e5a5b433b7bc3fd9ef1910
wolfskychina <wolfskychina@126.com>
2024-10-23 02:17:51 +0800

    h

--- src/leetcode/util/NestedIntegerMock.java
+++ src/leetcode/util/NestedIntegerMock.java
@@ -12,31 +12,26 @@ public class NestedIntegerMock implements NestedInteger{
 
     @Override
     public boolean isInteger() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isInteger'");
     }
 
     @Override
     public Integer getInteger() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getInteger'");
     }
 
     @Override
     public void setInteger(int value) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setInteger'");
     }
 
     @Override
     public void add(NestedInteger ni) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
 
     @Override
     public List<NestedInteger> getList() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getList'");
     }
 
