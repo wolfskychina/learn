fdb92b770278505f8d53b71efcc420219b01c2bc
wolfsky <wolfskychina@126.com>
2024-12-24 17:39:57 +0800

    office

--- src/leetcode/a700/_745Solution.java
+++ src/leetcode/a700/_745Solution.java
@@ -28,14 +28,14 @@ public class _745Solution {
         public int f(String pref, String suff) {
 
             List<Integer> l1 = ptree.query(pref);
-            List<Integer> l2 = stree.query(suff);
+            List<Integer> l2 = stree.rquery(suff);
             if (l1 == null || l2 == null)
                 return -1;
             int i = l1.size() - 1;
             int j = l2.size() - 1;
             while (i >= 0 && j >= 0) {
                 // TODO Integer不能直接比大小
-                if (l1.get(i).compareTo(l2.get(j))==0) {
+                if (l1.get(i).compareTo(l2.get(j)) == 0) {
                     return l1.get(i);
                 } else if (l1.get(i) > l2.get(j)) {
                     i--;
@@ -81,7 +81,7 @@ public class _745Solution {
                         Trie node = new Trie();
                         p.next[ss[i] - 'a'] = node;
                     }
-                    p.next[ss[i]-'a'].list.add(idx);
+                    p.next[ss[i] - 'a'].list.add(idx);
                     p = p.next[ss[i] - 'a'];
 
                 }
@@ -100,13 +100,28 @@ public class _745Solution {
                 }
                 return p.list;
             }
+
+            public List<Integer> rquery(String s) {
+
+                Trie p = this;
+                char[] ss = s.toCharArray();
+                for (int i=ss.length-1;i>=0;i--) {
+
+                    if (p.next[ss[i] - 'a'] == null)
+                        return null;
+                    else
+                        p = p.next[ss[i] - 'a'];
+
+                }
+                return p.list;
+            }
         }
     }
 
-    public static void main(String [] args){
-        String[] words = {"abbba","abba"};
+    public static void main(String[] args) {
+        String[] words = { "abbba", "abba" };
         _745Solution so = new _745Solution();
         WordFilter wf = so.new WordFilter(words);
-        wf.f("ab","ba");
+        wf.f("ab", "ba");
     }
 }
