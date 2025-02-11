bd8d892b1cc99f57f3be06308484d50d568ba620
wolfsky <wolfskychina@126.com>
2025-02-11 17:45:30 +0800

    office

---
+++ src/leetcode/a3000/_3403Solution.java
@@ -0,0 +1,23 @@
+package leetcode.a3000;
+
+/**
+ * 
+ */
+public class _3403Solution {
+
+    public String answerString(String s, int k) {
+        if (k == 1) {
+            return s;
+        }
+        int n = s.length();
+        String ans = "";
+        for (int i = 0; i < n; i++) {
+            String sub = s.substring(i, Math.min(i + n - k + 1, n));
+            if (sub.compareTo(ans) > 0) {
+                ans = sub;
+            }
+        }
+        return ans;
+    }
+
+}
