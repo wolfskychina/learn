a25b4819a0d7f34bd94b7234689c124038a27227
wolfskychina <wolfskychina@126.com>
2025-02-28 00:37:02 +0800

    home

--- src/leetcode/a2000/_2130Solution.java
+++ src/leetcode/a2000/_2130Solution.java
@@ -9,11 +9,16 @@ import leetcode.util.ListNode;
  */
 public class _2130Solution {
 
+    /**
+     * 快慢指针将列表对半分，同时对慢指针反转列表
+     * 总的翻转次数是n/2-1次
+     */
     public int pairSum(ListNode head) {
         ListNode prev = null, curr = head, last = head;
-        /**
-         * 快慢指针将列表对半分，同时对慢指针反转列表
-         * 总的翻转次数是n/2-1次
+
+        /*
+         * 不使用dummy，不提前移动节点，也可以进行头插
+         * 头部节点初始化为null，这样已经插入的队尾是断开的
          */
         while (last != null) {
             last = last.next.next;
