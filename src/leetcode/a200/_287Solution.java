8f2ab57e004546293e09e23c3bf17b3fd561d1c0
wolfsky <wolfskychina@126.com>
2025-04-07 17:30:57 +0800

    office

--- src/leetcode/a200/_287Solution.java
+++ src/leetcode/a200/_287Solution.java
@@ -12,7 +12,9 @@ public class _287Solution {
      * 直接在解空间中[0,n-1]进行二分搜索
      * 对于每一个mid值，再遍历原数组，找到小于等于mid值的元素的个数count
      * 如果count大于mid值，说明存在比mid小的值，重复了两次或以上
+     * 采用猜数字的策略，速度比较慢
      * {binary search}
+     * 
      * @param nums
      * @return
      */
@@ -42,5 +44,54 @@ public class _287Solution {
             }
         }
         return left;
+    }
+
+    /**
+     * 更快的做法，但是违反了题目要求不能修改原数组的值
+     * @param nums
+     * @return
+     */
+    public int findDuplicate1(int[] nums) {
+        // O(n) O(n)
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i + 1) {
+                if (nums[i] == nums[nums[i] - 1]) {
+                    return nums[i];
+                } else {
+                    // 把不对的值换到对的位置
+                    swap(nums, i, nums[i] - 1);
+                }
+            }
+        }
+        return -1;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    /**
+     * 快慢指针的方法
+     * @param nums
+     * @return
+     */
+    public int findDuplicate2(int[] nums) {
+        if(nums == null || nums.length ==0){
+            return -1;
+        }
+        int slow =nums[0],fast=nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 }
